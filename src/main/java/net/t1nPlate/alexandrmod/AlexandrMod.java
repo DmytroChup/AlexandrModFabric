b241abaee0f265bd215d94ee8768435cf692c679
Dmytro Chupryna <dmytro.chupryna18@gmail.com>
2023-01-25 15:39:51 +0200

    Initial commit

---
+++ src/main/java/net/t1nPlate/alexandrmod/AlexandrMod.java
@@ -0,0 +1,15 @@
+package net.t1nPlate.alexandrmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AlexandrMod implements ModInitializer {
+	public static final String MOD_ID = "alexandrmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/t1nPlate/alexandrmod/AlexandrModClient.java
@@ -0,0 +1,10 @@
+package net.t1nPlate.alexandrmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AlexandrModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
